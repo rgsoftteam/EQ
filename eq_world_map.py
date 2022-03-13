18f6faf1fb9e901875731fce855fbd918bc175c6
RGsoftteam <rgsoftteam@gmail.com>
2022-03-13 20:28:39 +0300

    0.1

--- eq_world_map.py
+++ eq_world_map.py
@@ -1,5 +1,8 @@
 import json
 
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
 # изучение структуры данных
 filename = 'data/eq_data_1_day_m1.json'
 with open(filename) as f:
@@ -20,7 +23,14 @@ for eq_dict in all_eq_dicts:
 	lons.append(lon)
 	lats.append(lat)
 
-print(mags[:10])
-print(lons[:5])
-print(lats[:5])
+# нанесение данных на карту
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data' : data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
+
+
+
+
 
